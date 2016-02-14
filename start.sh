2525be0f7d1517a563c36e4396f7ea94b19376d2
Mohammad Parvin <m.parvin986@gmail.com>
2016-02-14 14:17:33 +0000

    Update start.sh
    Add key press support

--- start.sh
+++ start.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 tcounter=0
+key_press=''
 while true
 do
   echo -en "$tcounter\r"
-  tcounter=$((tcounter+1))
-  sleep 0.99
+  read -n1 -t1 key_press
+  if [ -n "$key_press" ]
+        then
+                clear
+                echo -en "$tcounter\t"
+                exit
+        else
+                #echo -en "$tcounter\r"
+                tcounter=$((tcounter+1))
+        fi
 done
